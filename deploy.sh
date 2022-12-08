417c4c8d97d6637ebbf69ed18a354a5fe5272816
Abby Ng <abigailjng@gmail.com>
2022-12-07 22:43:35 -0500

    change deploy script for this project

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:abbyjng/abbyjng.github.io.git master:gh-pages
+git push -f git@github.com:abbyjng/chucks-donuts.git master:gh-pages
 cd -
